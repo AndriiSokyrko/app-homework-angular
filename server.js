c489420abd6a16623f5b5a843b12cb5f2dd69681
AndriiSokyrko <sokyrko.andrii@gmail.com>
2022-11-20 11:51:55 +0200

    server deploy commit #10

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/hw-angular'));
-
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/hw-angular/index.html'));
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./Json-server/db.json');
+const middlewares = jsonServer.defaults({
+  static: './dist/hw-angular'
+});
+const PORT = process.env.PORT || 3000;
+server.use(middlewares);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+}))
+server.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/hw-angular/'}),
+);
+server.use(router);
+server.listen(PORT, () => {
+  console.log('Server is running');
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
