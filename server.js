22e94356f16e94fdcbf8fcb85e5053d1ea5ab690
AndriiSokyrko <sokyrko.andrii@gmail.com>
2022-11-20 14:09:34 +0200

    server deploy commit #10

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./Json-server/db.json');
-const middlewares = jsonServer.defaults({
-  static: './dist/hw-angular'
-});
-const PORT = process.env.PORT || 3000;
-server.use(middlewares);
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-}))
-server.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/hw-angular/'}),
-);
-server.use(router);
-server.listen(PORT, () => {
-  console.log('Server is running');
-});
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('./Json-server/db.json')
 
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`)
+})
--- src/app/env.ts
+++ src/app/env.ts
@@ -1,2 +1,2 @@
 // export const  path = 'http://localhost:3000'
-export const path='https://app-homwork-angular.herokuapp.com/api'
+export const path='https://app-homwork-angular.herokuapp.com'
