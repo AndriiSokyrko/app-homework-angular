11e5ae18aacb1b93956352912033cf2451c50ef3
AndriiSokyrko <sokyrko.andrii@gmail.com>
2022-11-21 11:25:57 +0200

    server deploy commit #12

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./Json-server/db.json");
 const middlewares = jsonServer.defaults({ static: "./dist/hw-angular" });
-const port = 3000;
+const port = 8080;
 
 server.use(middlewares);
 server.use(jsonServer.rewriter({
