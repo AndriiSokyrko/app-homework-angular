4a742571b4b351000cfb005ee8145880033208eb
AndriiSokyrko <sokyrko.andrii@gmail.com>
2022-11-21 10:51:57 +0200

    server deploy commit #12

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 // const {PORT} = require("./src/app/env.ts");
 const server = jsonServer.create();
 const router = jsonServer.router("Json-server/db.json");
-const middlewares = jsonServer.defaults({ static: "./build" });
+const middlewares = jsonServer.defaults({ static: "./dist/hw-angular" });
 const port = 3000;
 
 server.use(middlewares);
